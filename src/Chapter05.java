d0e27d24354a07386e1d7b3d0a02bbed0d8c943f
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-03 17:07:51 +0900

    Chapter05確認問題

---
+++ src/Chapter05.java
@@ -0,0 +1,16 @@
+
+public class Chapter05 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String name = "Yamada";
+		System.out.println(name);
+		
+		name = "Suzuki";
+		System.out.println(name);
+		
+		final int LuckyNumber = 7;
+		System.out.println(LuckyNumber);
+	}
+
+}
