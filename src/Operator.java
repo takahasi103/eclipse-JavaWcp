2af0f70b0ca88b92ed9bcd742408b4a01a9eef1f
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-04 20:55:28 +0900

    比較・論理演算子

--- src/Operator.java
+++ src/Operator.java
@@ -30,6 +30,48 @@ public class Operator {
 		System.out.println("d:" + d);
 		System.out.println("g:" + g);
 		
+		// 代入演算子
+		int h = 10;
+		System.out.println(h += 5); // h+5の結果を代入
+		System.out.println(h -= 3); // h-3の結果を代入
+		System.out.println(h *= 2); // h*2の結果を代入
+		System.out.println(h /= 3); // h/3の結果を代入
+		System.out.println(h %= 2); //　h%2の結果を代入
+		
+		// 関係（比較）演算子
+		int i = 5;
+		boolean j;
+		j = i < 5; // i が 5 より小さい場合に true
+		System.out.println(j);
+		
+		j = i <= 5; // i が 5 より小さいか等しい場合に true
+		System.out.println(j);
+		
+		j = i > 8; // i が 8 より大きい場合に true
+		System.out.println(j);
+		
+		j = i >= 3; // i が 3 より大きいか等しい場合に true
+		System.out.println(j);
+		
+		j = i == 5; // i と 5 が等しい場合に true
+		System.out.println(j);
+		
+		j = i != 5; // i と 5 が等しくない場合に true
+		System.out.println(j);
+		
+		// 論理演算子
+		boolean k = true;
+		boolean l = false;
+		boolean m;
+		
+		m = k && true; // 両方が true の場合に true
+		System.out.println(m);
+		
+		m = l || k; // 	どちらかが true の場合に true
+		System.out.println(m);
+		
+		m = !l; // 	l が false の場合に true
+		System.out.println(m);
 	}
 
 }
