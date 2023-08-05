31be701af165b8e416e8bad0c0da4088ec7202ac
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-05 19:50:49 +0900

    switch文

---
+++ src/Switch.java
@@ -0,0 +1,35 @@
+
+public class Switch {
+	// 列挙型
+		enum Vals {
+			EXCELLENT,
+			GOOD,
+			POOR,
+		};
+
+	public static void main(String[] args) {
+		int val = 2;
+		switch (val) {
+		case 3:
+			System.out.println("よくできました！");
+			break;
+		case 2:
+			System.out.println("ふつうでした！");
+			break;
+		default:
+			System.out.println("がんばろう！");
+		}
+		Vals val2 = Vals.EXCELLENT;
+		switch (val2) {
+		case EXCELLENT:
+			System.out.println("よくできました！");
+			break;
+		case GOOD:
+			System.out.println("ふつうでした！");
+			break;
+		default:
+			System.out.println("がんばろう！");
+		}
+	}
+
+}
