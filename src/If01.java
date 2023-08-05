57b7c3897e132fcb35ae3a1d040151dbbf795876
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-05 17:29:27 +0900

    slse if

--- src/If01.java
+++ src/If01.java
@@ -3,14 +3,24 @@ public class If01 {
 
 	public static void main(String[] args) {
 		int number = 7;
+//		if (number < 5) {
+//			System.out.println(number + " は5未満です");
+//		}
+//		if (number < 10) {
+//			System.out.println(number + " は10未満です");
+//		}
+//		if (number < 15) {
+//			System.out.println(number + " は15未満です");
+//		}
+		
 		if (number < 5) {
 			System.out.println(number + " は5未満です");
-		}
-		if (number < 10) {
-			System.out.println(number + " は10未満です");
-		}
-		if (number < 15) {
-			System.out.println(number + " は15未満です");
+		} else if (number < 10) {
+			System.out.println(number + " は5以上、10未満です");
+		} else if (number < 15) {
+			System.out.println(number + " は10以上、15未満です");
+		} else {
+			System.out.println(number + " は15未満でないです");
 		}
 	}
 
