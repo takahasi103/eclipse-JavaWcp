c5bb9d885150a6d8ef45faeae4ff0fa7c6217450
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-05 20:25:47 +0900

    確認問題08 問2

--- src/Chapter08.java
+++ src/Chapter08.java
@@ -21,7 +21,40 @@ public class Chapter08 {
 		} else {
 			System.out.println("とても遠いです");
 		}
-
+		
+	/*
+	 問題2
+	 曜日を表すString型の変数を定義し、 英単語で初期化する
+	 英単語に該当する日本語で曜日を出力する
+	 曜日以外の単語の場合は "曜日ではありません" を出力する
+	*/
+		
+		String week = "Monday";
+		switch (week) {
+		case "Sunday":
+			System.out.println("日曜日");
+			break;
+		case "Monday":
+			System.out.println("月曜日");
+			break;
+		case "Tuesday":
+			System.out.println("火曜日");
+			break;
+		case "Wednesday":
+			System.out.println("水曜日");
+			break;
+		case "Thursday":
+			System.out.println("木曜日");
+			break;
+		case "Firday":
+			System.out.println("金曜日");
+			break;
+		case "Staurday":
+			System.out.println("土曜日");
+			break;
+		default:
+			System.out.println("曜日ではありません");
+		}
 	}
 
 }
