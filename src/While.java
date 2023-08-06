2492923254f75a40b2a9517b2f48c2ec5b200554
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-06 15:39:12 +0900

    do-while文

--- src/While.java
+++ src/While.java
@@ -2,12 +2,18 @@
 public class While {
 
 	public static void main(String[] args) {
-		int number = 1;
+		int number = 100;
 		// while文
         while (number < 50) {
             number *= 2;
             System.out.println("While01 = " + number);
         }
+        
+        // do-while文
+        do {
+            number *= 2;
+            System.out.println("DoWhile01 = " + number);
+        } while (number < 50);
 	}
 
 }
