9c439ddcbbece91243999d1fd401db0cc496f7d2
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-06 15:50:18 +0900

    再帰処理

--- src/For.java
+++ src/For.java
@@ -27,6 +27,20 @@ public class For {
         }
             System.out.println("Continue01 = " + count);
         }
+        
+        // 再帰処理
+        int number = 1;
+        printNum(number);
 	}
+	
+	public static int printNum(int number) {
+        if (number < 50) {
+            number *= 2;
+            System.out.println("While01 = " + number);
+            // 条件にマッチしたら自分自身を再度呼び出す
+            printNum(number);
+        }
+        return number;
+    }
 
 }
