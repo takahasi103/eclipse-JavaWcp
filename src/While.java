cedb6933e5b0e89c1a84b4e7517793aa6a10b3a1
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-06 15:34:49 +0900

    while文

---
+++ src/While.java
@@ -0,0 +1,13 @@
+
+public class While {
+
+	public static void main(String[] args) {
+		int number = 1;
+		// while文
+        while (number < 50) {
+            number *= 2;
+            System.out.println("While01 = " + number);
+        }
+	}
+
+}
