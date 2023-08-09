237beca0fd248e63efcd7cf38110723c91b3a224
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-09 16:03:22 +0900

    defaultメソッド

--- src/extend/phone/Phone.java
+++ src/extend/phone/Phone.java
@@ -2,8 +2,17 @@ package extend.phone;
 
 public interface Phone {
 	// フィールドの宣言（定数）
-	int MAX_NUMBER_DIGITS = 11;
+//	int MAX_NUMBER_DIGITS = 11;
 
 	// public abstractが自動で追加される（抽象メソッド）
+//	void call(String number);
+	
+	final int MAX_NUMBER_DIGITS = 11;
+
 	void call(String number);
+
+	// defaultメソッドの宣言
+	default void powerOff() {
+		System.out.println("電源を切ります。");
+	}
 }
