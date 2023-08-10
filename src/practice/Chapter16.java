b4f5c0dcdd5d7d863a3944b81f0b25bfeb29aadb
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-10 12:18:05 +0900

    確認問題16 スタート

---
+++ src/practice/Chapter16.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Chapter16 {
+	public static void main(String[] args) {
+		// 5つのサブクラスのインスタンスを生成
+		Human[] humans = ・・・・
+
+		for (Human h : humans) {
+			// workメソッドを呼び出す
+
+			// Programmerクラスの場合、studyメソッドを呼ぶ
+		}
+	}
+}
