69a5b26cde8d1c09e94811139eb17aef9c33afb3
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-12 11:10:03 +0900

    Dragonクラス作成

--- src/rpg/living/monsters/Dragon.java
+++ src/rpg/living/monsters/Dragon.java
@@ -1,5 +1,11 @@
 package rpg.living.monsters;
 
-public class Dragon extends Monster {
+import rpg.Dice;
 
+public class Dragon extends Monster {
+	public Dragon(String name, String weapon) {
+		super(name, weapon);
+		setHp(Dice.get(270, 330));
+		setOffensive(Dice.get(12, 18));
+	}
 }
