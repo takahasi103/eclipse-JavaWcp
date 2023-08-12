b74509822bd8b3c5cf36d28eaaddc02cf17240fc
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-12 10:29:40 +0900

    Wizardクラス作成

--- src/rpg/living/humans/Wizard.java
+++ src/rpg/living/humans/Wizard.java
@@ -1,5 +1,11 @@
 package rpg.living.humans;
 
-public class Wizard extends Human {
+import rpg.Dice;
 
+public class Wizard extends Human {
+	public Wizard(String name, String weapon) {
+		super(name, weapon);
+		setHp(Dice.get(120, 180));
+		setOffensive(Dice.get(12, 18));
+	}
 }
