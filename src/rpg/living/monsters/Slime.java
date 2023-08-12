33b7f9b93734976a134aeb63fa3e6909f810032b
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-12 11:03:59 +0900

    Slimeクラス作成

--- src/rpg/living/monsters/Slime.java
+++ src/rpg/living/monsters/Slime.java
@@ -1,5 +1,11 @@
 package rpg.living.monsters;
 
-public class Slime extends Monster {
+import rpg.Dice;
 
+public class Slime extends Monster {
+	public Slime(String name, String weapon) {
+		super(name, weapon);
+		setHp(Dice.get(70, 130));
+		setOffensive(Dice.get(5, 11));
+	}
 }
