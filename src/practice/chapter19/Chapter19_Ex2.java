decd62eb535d00713a4afd04b24a77dfdace70ca
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-14 16:13:10 +0900

    確認問題19-2 解答

--- src/practice/chapter19/Chapter19_Ex2.java
+++ src/practice/chapter19/Chapter19_Ex2.java
@@ -4,20 +4,26 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
+
 public class Chapter19_Ex2 {
-	public static void main(String[] args) {
+	public static void main(String[] args)   {
 		// ファイルを開き、loadメソッドでプロパティを読み込む
 		try (FileReader fr = new FileReader("C:\\pleiades\\workspace\\personal.properties")) {
 			Properties p = new Properties();
 			p.load(fr);
 
 			// 各設定内容を出力
-
-			// 例外処理を記述
+			System.out.println("名前:" + p.getProperty("name"));
+			System.out.println("年齢:" + p.getProperty("age"));
+			System.out.println("性別:" + p.getProperty("gender"));
+		} catch (FileNotFoundException e) {
+			System.out.println("ファイルがありませんでした");
 			// printStackTraceで例外を出力
-		} catch (・・・) {
-			・・・
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("エラーです");
+			// printStackTraceで例外を出力
+			e.printStackTrace();
 		}
-	}
 }
 
