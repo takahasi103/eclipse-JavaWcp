812195663200710cd9f91e18928b059cfce5d960
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-17 16:46:04 +0900

    確認問題(応用)3 終了

--- src/api/Chapter23_Ex1.java
+++ src/api/Chapter23_Ex1.java
@@ -12,6 +12,30 @@ package api;
 //	24(日) 25(月) 26(火) 27(水) 28(木) 29(金) 30(土)
 //	31(日)
 
-public class Chapter23_Ex1 {
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
+public class Chapter23_Ex1 {
+	public static void main(String[] args) throws Exception {
+		// 当日のインスタンスからwithDayOfMonthでその月の1日のインスタンスを得る
+		LocalDate d = LocalDate.now().withDayOfMonth(1);
+
+		// lengthOfMonthでその月の最後の日が取得できる
+		final int lastDay = d.lengthOfMonth();
+		for (int i = 1; i <= lastDay; i++) {
+			// 曜日を取得
+			DayOfWeek w = d.getDayOfWeek();
+			// 曜日の文字列表現を取得
+			String wstr = w.getDisplayName(TextStyle.SHORT, Locale.getDefault());
+			System.out.print(d.getDayOfMonth() + "(" + wstr + ") ");
+			// 土曜日かどうかを判定
+			if (w == DayOfWeek.SATURDAY) {
+				System.out.println();
+			}
+
+			d = d.plusDays(1);
+		}
+	}
 }
