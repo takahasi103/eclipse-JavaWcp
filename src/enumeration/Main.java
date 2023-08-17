fd9159abe6e5299620d1183ff600e01c0ab4ba03
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-17 15:41:21 +0900

    列挙型の活用

--- src/enumeration/Main.java
+++ src/enumeration/Main.java
@@ -6,6 +6,10 @@ public class Main {
 		System.out.println(Dessert.APPLE);
 		System.out.println(Dessert.ICE_CREAM);
 		System.out.println(Dessert.CAKE);
+		
+		Dessert2.APPLE.advertise(); // 「まっかなりんごが100円！」と出力
+		Dessert2.ICE_CREAM.advertise(); // 「夏にぴったりアイスクリームが200円！」と出力
+		Dessert2.CAKE.advertise(); // 「皆大好き甘いケーキが300円！」と出力
 	}
 
 }
