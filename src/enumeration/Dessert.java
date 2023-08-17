aa3d865a57de03a4fb5f08bdf8ec8f9ec24a9d41
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-17 15:34:42 +0900

    列挙型とは

---
+++ src/enumeration/Dessert.java
@@ -0,0 +1,11 @@
+package enumeration;
+
+public enum Dessert {
+//	例えば食後のデザートを列挙型を使わずに定数で定義すると、
+//	public static final String APPLE = "りんご";
+//	public static final String ICE_CREAM = "アイスクリーム";
+//	public static final String CAKE = "ケーキ";
+//	となりますが、列挙型で書くと次のように簡潔に書くことができます。
+	
+	APPLE, ICE_CREAM, CAKE
+}
---
+++ src/enumeration/Main.java
@@ -0,0 +1,11 @@
+package enumeration;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(Dessert.APPLE);
+		System.out.println(Dessert.ICE_CREAM);
+		System.out.println(Dessert.CAKE);
+	}
+
+}
