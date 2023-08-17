43735e83a94a2c6cf753aae95c3c91f8590b92bf
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-17 15:47:49 +0900

    確認問題(応用)2 開始

---
+++ src/enumeration/Chapter22.java
@@ -0,0 +1,19 @@
+package enumeration;
+
+import java.util.Scanner;
+
+//	確認問題
+//	列挙型「DayOfWeek」を「Sun, Mon, Tue, Wed, Thu, Fri, Sat」と定義してください。
+//	
+//	キーボードからの入力で曜日名を受け取り、該当するenumインスタンスを取得して、
+//	曜日の日本語名を出力してください。
+
+public class Chapter22 {
+
+	public static void main(String[] args) {
+		System.out.print("曜日を入力して下さい：");
+	    String week = new Scanner(System.in).nextLine();
+	    // 以下に処理を記述
+	}
+
+}
