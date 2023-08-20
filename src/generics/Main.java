511e52ad526855dacfa24326b6f2a71682e25aee
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-20 15:28:24 +0900

    確認問題2

--- src/generics/Main.java
+++ src/generics/Main.java
@@ -1,7 +1,11 @@
 package generics;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
 
@@ -42,6 +46,16 @@ public class Main {
 
 		wildCardIntList = numList; //OK
 //		intList = numList; //コンパイルエラー
+		
+		// 確認問題2
+		Set<Integer> set = new LinkedHashSet<>();
+		set.add(1); // OK
+//		set.add("Two"); // コンパイルエラー
+
+		Map<Integer, String> map = new HashMap<>();
+		map.put(1, "Taro"); // OK
+		map.put(2, "Hanako"); // OK
+//		int value = map.get(0); // コンパイルエラー
 
 	}
 
