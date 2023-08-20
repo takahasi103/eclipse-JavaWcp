c77c5cc9c056fae410f6a4bb0aecbc56d4710b93
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-20 14:21:02 +0900

    Taskクラス作成

--- src/practice/chapter25/Task.java
+++ src/practice/chapter25/Task.java
@@ -1,5 +1,21 @@
 package practice.chapter25;
 
-public class Task {
+import java.time.LocalDate;
 
+public class Task {
+	private LocalDate date;
+    private String content;
+
+    public Task(LocalDate date, String content) {
+        this.date = date;
+        this.content = content;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
