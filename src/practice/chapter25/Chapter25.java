fd51d0dd202fc319fd72559cfa630c7f69c0f1d3
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-20 14:16:20 +0900

    課題 開始

---
+++ src/practice/chapter25/Chapter25.java
@@ -0,0 +1,10 @@
+package practice.chapter25;
+
+public class Chapter25 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/practice/chapter25/Task.java
@@ -0,0 +1,5 @@
+package practice.chapter25;
+
+public class Task {
+
+}
