cfd67ef2492723abd09d44135dfd4f75dc7e9694
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-22 14:07:32 +0900

    確認問題28 終了

--- src/lambda_expression/Chapter28.java
+++ src/lambda_expression/Chapter28.java
@@ -2,9 +2,17 @@ package lambda_expression;
 
 public class Chapter28 {
 	
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+	public static void main(String[] args) throws Exception {
+		Func1 f1 = x -> x % 2 == 1;
+		Func2 f2 = (male, name) -> {
+			if (male) {
+				return "Mr." + name;
+			} else {
+				return "Ms." + name;
+			}
+		};
+		System.out.println(f1.isOdd(15));
+		System.out.println(f2.addNamePrefix(true, "Smith"));
 	}
 
 }
