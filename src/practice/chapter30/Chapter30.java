302168ff5b6a89b2920888a772405b89c7390c60
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-23 15:43:32 +0900

    確認問題30 開始

---
+++ src/practice/chapter30/Chapter30.java
@@ -0,0 +1,10 @@
+package practice.chapter30;
+
+public class Chapter30 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ src/practice/chapter30/Task.java
@@ -0,0 +1,5 @@
+package practice.chapter30;
+
+public class Task {
+
+}
