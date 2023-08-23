ef5cd4de6642d1ec22ce44c71559d12d17cc1317
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-23 15:55:57 +0900

    未完了のタスクの個数をカウント

--- src/practice/chapter30/Chapter30.java
+++ src/practice/chapter30/Chapter30.java
@@ -7,11 +7,20 @@ import java.util.List;
 public class Chapter30 {
 	public static void main(String[] args) throws Exception {
 		List<Task> list = new ArrayList<>();
-        list.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う", true));
-        list.add(new Task(LocalDate.of(2021, 9, 15), "○○社面接", false));
-        list.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う", true));
-        list.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く", false));
-        list.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く", false));
+		list.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う", true));
+		list.add(new Task(LocalDate.of(2021, 9, 15), "○○社面接", false));
+		list.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う", true));
+		list.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く", false));
+		list.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く", false));
+		
+		// 未完了のタスクの個数をカウント
+        int unfinishedCount = 0;
+        for (Task task : list) {
+            if (!task.isDone()) {
+                unfinishedCount++;
+            }
+        }
+        System.out.println("未完了のタスクの個数は" + unfinishedCount);
 
 	}
 
