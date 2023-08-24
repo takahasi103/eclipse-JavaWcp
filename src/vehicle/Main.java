7a0231e7cad90509ca78eb00aeee039800267802
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-24 13:07:49 +0900

    Bicycleインスタンス作成

--- src/vehicle/Main.java
+++ src/vehicle/Main.java
@@ -3,9 +3,12 @@ package vehicle;
 public class Main {
 
 	public static void main(String[] args) {
-		Car car1 = new Car("サンプル", "赤");
-		
+		Car car1 = new Car("自動車", "赤");
 		car1.printData();
+		
+		Bicycle bicycle1 = new Bicycle("自転車", "青");
+		bicycle1.printData();
+		
 
 	}
 
