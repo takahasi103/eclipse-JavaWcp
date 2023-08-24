377ff473d28d7458648d6513234a946e6e8fb3b4
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-24 13:15:03 +0900

    bicycleクラスのrunメソッド

--- src/vehicle/Main.java
+++ src/vehicle/Main.java
@@ -6,11 +6,22 @@ public class Main {
 		Car car1 = new Car("自動車", "赤");
 		car1.printData();
 		
+		System.out.println();
+		
 		Bicycle bicycle1 = new Bicycle("自転車", "青");
 		bicycle1.printData();
 		
+		System.out.println();
+		
+		System.out.println(car1.getName());
 		car1.run(20);
 		car1.printData();
+		
+		System.out.println();
+		
+		System.out.println(bicycle1.getName());
+		bicycle1.run(10);
+		bicycle1.printData();
 
 	}
 
