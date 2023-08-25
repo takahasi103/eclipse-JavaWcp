4f8db4137f13493603e9108bcebb1f2be3d00650
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-25 12:26:25 +0900

    Bikeインスタンス作成

--- src/vehicle/Main.java
+++ src/vehicle/Main.java
@@ -26,56 +26,59 @@ public class Main {
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.print("車種:");
-		String carModle = scanner.next();
-		System.out.print("車の色:");
-		String carColor = scanner.next();
+//		System.out.print("車種:");
+//		String carModle = scanner.next();
+//		System.out.print("車の色:");
+//		String carColor = scanner.next();
+//		
+//		System.out.println();
+//		
+//		Car car2 = new Car(carModle, carColor);
+//		car2.printData();
+//		
+//		System.out.println("何km走りますか？");
+//		int carDistance = scanner.nextInt();
+//		car2.run(carDistance);
+//		
+//		car2.checkFuel();
+//		System.out.println("何L給油しますか?");
+//		int carFuel = scanner.nextInt();
+//		car2.charge(carFuel);
+//		
+//		System.out.println();
+//		
+//		System.out.println("色を変更しますか？[Yes or No]");
+//		String Answer = scanner.next();
+//		switch (Answer) {
+//		case "Yes":
+//			System.out.println("何色にしますか？");
+//			String newColor = scanner.next();
+//			car2.setColor(newColor);
+//			car2.printData();
+//			break;
+//		case "No":
+//			System.out.println("変更をしません");
+//			break;
+//		default:
+//			System.out.println("YesかNoで選択してください");
+//			break;
+//		}
+//		
+//		System.out.println("車種を変更しますか？[Yes or No]");
+//		Answer = scanner.next();
+//		if (Answer.equals("Yes")) {
+//			System.out.println("車種を入力してください");
+//			String newModle = scanner.next();
+//			car2.setModle(newModle);
+//			car2.printData();
+//		} else if (Answer.equals("No")) {
+//			System.out.println("変更をしません");
+//		} else {
+//			System.out.println("YesかNoで選択してください");
+//		}
 		
-		System.out.println();
-		
-		Car car2 = new Car(carModle, carColor);
-		car2.printData();
-		
-		System.out.println("何km走りますか？");
-		int carDistance = scanner.nextInt();
-		car2.run(carDistance);
-		
-		car2.checkFuel();
-		System.out.println("何L給油しますか?");
-		int carFuel = scanner.nextInt();
-		car2.charge(carFuel);
-		
-		System.out.println();
-		
-		System.out.println("色を変更しますか？[Yes or No]");
-		String Answer = scanner.next();
-		switch (Answer) {
-		case "Yes":
-			System.out.println("何色にしますか？");
-			String newColor = scanner.next();
-			car2.setColor(newColor);
-			car2.printData();
-			break;
-		case "No":
-			System.out.println("変更をしません");
-			break;
-		default:
-			System.out.println("YesかNoで選択してください");
-			break;
-		}
-		
-		System.out.println("車種を変更しますか？[Yes or No]");
-		Answer = scanner.next();
-		if (Answer.equals("Yes")) {
-			System.out.println("車種を入力してください");
-			String newModle = scanner.next();
-			car2.setModle(newModle);
-			car2.printData();
-		} else if (Answer.equals("No")) {
-			System.out.println("変更をしません");
-		} else {
-			System.out.println("YesかNoで選択してください");
-		}
+		Bike bike1 = new Bike("テスト", "黒");
+		bike1.printData();
 
 	}
 
