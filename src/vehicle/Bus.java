8b3d85ed68962f464c0e464ea0c3d76be5812a46
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-08-29 13:21:25 +0900

    Busクラス getPassengerメソッド作成

--- src/vehicle/Bus.java
+++ src/vehicle/Bus.java
@@ -1,9 +1,14 @@
 package vehicle;
 
 public class Bus extends Car {
-	private int passenger;
+	private int passenger = 0;
 	
 	Bus(String name, String color) {
 		super(name, color);
 	}
+	
+	public int getPassenger() {
+		return this.passenger;
+	}
+	
 }
