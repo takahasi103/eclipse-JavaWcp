310a2724e8be3d3d41020ab41db690cf6b6d821a
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-09-06 21:40:31 +0900

    共通処理をまとめる方法

--- src/unittest/CalculatorTest.java
+++ src/unittest/CalculatorTest.java
@@ -2,6 +2,7 @@ package unittest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -16,43 +17,75 @@ class CalculatorTest {
 //		fail("まだ実装されていません");
 //	}
 	
-	 @Test
+//	 @Test
 //	  org.junit.jupiter.api.DisplayNameアノテーションで、テスト結果表示画面に使う文言を設定できる
 //	  DisplayNameアノテーションなしの場合は、テストメソッド名が表示される
-	 @DisplayName("2の二乗は4になる")
-	 void testSquare2() {
+//	 @DisplayName("2の二乗は4になる")
+//	 void testSquare2() {
 
 //	   1. テスト対象クラスのセットアップとして、インスタンス化や必要に応じて初期値のセットなどを行う
-	  Calculator calc = new Calculator();
+//	  Calculator calc = new Calculator();
 
 //	   2. テスト対象メソッド（今回はsquareメソッド）を実行し、結果を取得する
-	  int result = calc.square(2);
+//	  int result = calc.square(2);
 
 //	   3. 実行結果の実測値と期待値の検証（アサーション）を行う
 //	   ここでは実測値（result）が期待結果（4）と一致するかどうかを検証
-	  assertEquals(result, 4);
-	 }
+//	  assertEquals(result, 4);
+//	 }
 	 
-	 @Test
-	 @DisplayName("4の平方根は2になる")
-	 void testRoot4() {
+//	 @Test
+//	 @DisplayName("4の平方根は2になる")
+//	 void testRoot4() {
 //	   1. テスト対象クラスのセットアップ
-	  Calculator calc = new Calculator();
+//	  Calculator calc = new Calculator();
 //	   2. テスト対象メソッド（今回はrootメソッド）を実行し、結果を取得する
-	  double result = calc.root(4.0);
+//	  double result = calc.root(4.0);
 //	   3. 実行結果の実測値と期待値の検証（アサーション）
-	  assertEquals(result, 2.0);
-	 }
+//	  assertEquals(result, 2.0);
+//	 }
 	 
-	 @Test
-	 @DisplayName("-1の平方根は例外が発生する")
-	 void testRootException() {
+//	 @Test
+//	 @DisplayName("-1の平方根は例外が発生する")
+//	 void testRootException() {
 //	   1. テスト対象クラスのセットアップ
-	  Calculator calc = new Calculator();
+//	  Calculator calc = new Calculator();
 //	   2. テスト対象メソッドの実行と、3. 実行結果の検証を1行で行う
 //	   org.junit.jupiter.api.Assertions.assertThrowsメソッドを使用する
-	  assertThrows(IllegalArgumentException.class, () -> calc.root(-1));
-	 }
+//	  assertThrows(IllegalArgumentException.class, () -> calc.root(-1));
+//	 }
 	
+//	共通処理をまとめる
+//	 テスト対象クラスをフィールドに追加
+	private Calculator calc;
+	
+//	 各テストメソッドの実行前に毎回呼ばれる処理を記載する
+	@BeforeEach
+	void setUp() {
+//		 フィールドにCalculatorクラスのインスタンスをセットする
+		calc = new Calculator();
+	}
+
+	@Test
+	@DisplayName("2の二乗は4になる")
+	void testSquare2() {
+		int result = calc.square(2);
+		assertEquals(result, 4);
+	}
+
+	@Test
+	@DisplayName("4の平方根は2になる")
+	void testRoot4() {
+//		 calcフィールドを呼び出してテスト実行する
+		double result = calc.root(4.0);
+		assertEquals(result, 2.0);
+	}
+
+	@Test
+	@DisplayName("-1の平方根は例外が発生する")
+	void testRootException() {
+//		 calcフィールドを呼び出してテスト実行する
+		assertThrows(IllegalArgumentException.class, () -> calc.root(-1));
+	}
 
 }
