8c5a901478cae6d2eea2175e35fee55d15473361
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-09-06 21:31:45 +0900

    例外を検証するテストを実装

--- src/unittest/Calculator.java
+++ src/unittest/Calculator.java
@@ -5,4 +5,12 @@ public class Calculator {
 	public int square(int x) {
 		return x * x;
 	}
+	
+	// 引数に与えられた数値の平方根を返すメソッド
+	public double root(double x) {
+		if (x < 0) {
+			throw new IllegalArgumentException("負の値の平方根は求められません。");
+		}
+		return Math.sqrt(x);
+	}
 }
