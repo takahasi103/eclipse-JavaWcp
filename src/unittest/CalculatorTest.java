9c630f30c6f86ee8bcf388131a73c05d4ef06658
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-09-06 21:30:04 +0900

    テストの書き方

--- src/unittest/CalculatorTest.java
+++ src/unittest/CalculatorTest.java
@@ -2,13 +2,36 @@ package unittest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+//自動生成した場合アクセス修飾子はデフォルトだが、publicでも問題ない
+//テストクラスの名前は自由だが、xxxTestやTestxxxといった命名規則にすることが多い
 class CalculatorTest {
+//	 テストメソッドの目印としてTestアノテーションを付ける
+//	@Test
+//	void test() {
+//		 org.junit.jupiter.api.Assertions.failメソッドは、必ずテストを失敗させるメソッド。
+//       まだ自動で追加しただけなので、テストメソッドが未実装であることを示している
+//		fail("まだ実装されていません");
+//	}
+	
+	 @Test
+//	  org.junit.jupiter.api.DisplayNameアノテーションで、テスト結果表示画面に使う文言を設定できる
+//	  DisplayNameアノテーションなしの場合は、テストメソッド名が表示される
+	 @DisplayName("2の二乗は4になる")
+	 void testSquare2() {
 
-	@Test
-	void test() {
-		fail("まだ実装されていません");
-	}
+//	   1. テスト対象クラスのセットアップとして、インスタンス化や必要に応じて初期値のセットなどを行う
+	  Calculator calc = new Calculator();
+
+//	   2. テスト対象メソッド（今回はsquareメソッド）を実行し、結果を取得する
+	  int result = calc.square(2);
+
+//	   3. 実行結果の実測値と期待値の検証（アサーション）を行う
+//	   ここでは実測値（result）が期待結果（4）と一致するかどうかを検証
+	  assertEquals(result, 4);
+	 }
+	
 
 }
