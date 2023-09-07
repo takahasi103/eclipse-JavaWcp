c38368bffa8b808c1fcec15e155d4d1e0fd983dd
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-09-07 23:24:39 +0900

    テストクラスの作成

---
+++ src/mockitotest/MockitoSampleTest.java
@@ -0,0 +1,14 @@
+package mockitotest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MockitoSampleTest {
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
